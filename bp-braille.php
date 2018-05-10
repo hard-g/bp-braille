c2d16165f2896b2b9b9f4b3b20ef30773e8fbb09
George Mamadashvili <georgemamadashvili@gmail.com>
2018-05-10 18:41:54 +0400

    Adds better check for Braille configuration before initializing the plugin
    * Ref: #2

--- bp-braille.php
+++ bp-braille.php
@@ -16,24 +16,19 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
 
 	add_action( 'bp_loaded', __NAMESPACE__ . '\\bootstrap' );
-	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\assets' );
 }
 
 function bootstrap() {
-	// Bail if WP Braill plugin isn't active
-	if ( ! function_exists( 'get_braille' ) ) {
+	if ( ! braille_works() ) {
 		return;
 	}
 
 	Plugin::init();
+
+	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\assets' );
 }
 
 function assets() {
-	// Bail if WP Braill plugin isn't active
-	if ( ! function_exists( 'get_braille' ) ) {
-		return;
-	}
-
 	wp_enqueue_script( 'bp-braille', plugins_url( 'public/js/bp-braille.js', __FILE__ ), [ 'jquery' ], '0.1.0' );
 
 	$l10n = [
@@ -43,4 +38,39 @@ function assets() {
 		]
 	];
 	wp_localize_script( 'bp-braille', 'bpBraille', $l10n );
+}
+
+/**
+ * Checks Baille LibLouis server connection.
+ *
+ * @return bool
+ */
+function braille_works() {
+	$use_remote = get_option( 'braille_use_remote' );
+	$remote_url = get_option( 'braille_remote_url', '' );
+
+	if ( empty( $use_remote ) ) {
+		return false;
+	}
+
+	if ( empty( $remote_url ) ) {
+		return false;
+	}
+
+	$works = (bool) get_transient( 'braille_works' );
+
+	if ( false === $works ) {
+		$response = wp_remote_post( $remote_url, [
+			'timeout' => 45,
+			'headers' => [ 'Content-type: application/json' ],
+			'body' => json_encode( [ 'content' => 'Connection Test' ] ),
+			'cookies' => []
+		] );
+
+		$works = 200 === wp_remote_retrieve_response_code( $response );
+
+		set_transient( 'braille_works', $works, HOUR_IN_SECONDS );
+	}
+
+	return $works;
 }
